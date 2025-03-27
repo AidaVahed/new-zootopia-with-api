e0c5955c562fca2c418cb80d21056d33f4f6f494
aidavahed <aida.vahed@gmail.com>
2025-03-27 23:34:36 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -19,18 +19,20 @@ for animal in animals_data:
     animal_info = '<li class="cards__item">'
 
     if 'name' in animal:
-        animal_info += f"Name: {animal['name']}<br/>\n"
+        animal_info += f'<div class="card__title">{animal["name"]}</div>\n'
+
+    animal_info += '<p class="card__text">\n'
 
     if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        animal_info += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+        animal_info += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
 
     if 'locations' in animal and len(animal['locations']) > 0:
-        animal_info += f"Location: {animal['locations'][0]}<br/>\n"
+        animal_info += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
     if 'taxonomy' in animal and 'order' in animal['taxonomy']:
-        animal_info += f"Type: {animal['taxonomy']['order']}<br/>\n"
+        animal_info += f'<strong>Type:</strong> {animal["taxonomy"]["order"]}<br/>\n'
 
-    animal_info += '</li>'
+    animal_info += '</p>\n</li>'
     output += animal_info
 
 final_html = html_template.replace('__REPLACE_ANIMALS_INFO__', output)
