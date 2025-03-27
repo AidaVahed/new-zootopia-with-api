191ba7eaa8f0a09143774d3fc5ff17e8991ad0b9
aidavahed <aida.vahed@gmail.com>
2025-03-27 23:29:58 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,39 +1,42 @@
 import json
 
+
 def load_data(file_path):
-    """ Loads a JSON file """
+    """Loads a JSON file from the given file path."""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
 with open('animals_template.html', 'r') as template_file:
-  html_template = template_file.read()
+    """Reads the HTML template file."""
+    html_template = template_file.read()
 
 animals_data = load_data('animals_data.json')
 
 output = ''
 
 for animal in animals_data:
-  animal_info = '<li class="cards__item">'
+    animal_info = '<li class="cards__item">'
 
-  if 'name' in animal:
-    animal_info += f"<div class='card__title'>Name: {animal['name']}</div>"
+    if 'name' in animal:
+        animal_info += f"Name: {animal['name']}<br/>\n"
 
-  if 'characteristics' in animal and 'diet' in animal['characteristics']:
-    animal_info += f"<div class='card__text'>Diet: {animal['characteristics']['diet']}</div>"
+    if 'characteristics' in animal and 'diet' in animal['characteristics']:
+        animal_info += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
-  if 'locations' in animal and len(animal['locations']) > 0:
-    animal_info += f"<div class='card__text'>Location: {animal['locations'][0]}</div>"
+    if 'locations' in animal and len(animal['locations']) > 0:
+        animal_info += f"Location: {animal['locations'][0]}<br/>\n"
 
-  if 'taxonomy' in animal:
-    animal_info += f"<div class='card__text'>Type: {animal['taxonomy'].get('order', 'Unknown')}</div>"
+    if 'taxonomy' in animal and 'order' in animal['taxonomy']:
+        animal_info += f"Type: {animal['taxonomy']['order']}<br/>\n"
 
-  animal_info += '</li>'
-  output += animal_info
+    animal_info += '</li>'
+    output += animal_info
 
 final_html = html_template.replace('__REPLACE_ANIMALS_INFO__', output)
 
 with open('animals.html', 'w') as html_file:
-  html_file.write(final_html)
+    """Writes the generated HTML content to a new file."""
+    html_file.write(final_html)
 
 print("HTML file generated successfully: animals.html")
